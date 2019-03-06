7f197d1b43136d713ccf8ed00d917ee88631eee7
Shariff Inayat <shariff.inayat@gmail.com>
2019-03-06 17:39:50 +0000

    fix: Building of native modules with Node 10

--- web3-install-layer/docker-npm.js
+++ web3-install-layer/docker-npm.js
@@ -14,7 +14,7 @@ Usage:
 
 const childProcess = require('child_process')
 
-const nodejsImage = 'node:10.15.1'
+const nodejsImage = 'aws-linux-node-builder:10.15.2'
 const innerWorkingDir = '/src'
 const dockerArgs = [
   'run', '-i',
